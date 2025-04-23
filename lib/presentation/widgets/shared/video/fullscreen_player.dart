df8b4b48b5f9d5d673c4fd84d0a142522509299b
lucas juarez <lujuarez@emeal.nttdata.com>
2025-04-23 16:02:06 -0300

    add text into videos

--- lib/presentation/widgets/shared/video/fullscreen_player.dart
+++ lib/presentation/widgets/shared/video/fullscreen_player.dart
@@ -45,9 +45,37 @@ class _FullscreenPlayerState extends State<FullscreenPlayer> {
 
         return AspectRatio(
           aspectRatio: controller.value.aspectRatio,
-          child: VideoPlayer(controller),
+          child: Stack(
+            children: [
+              VideoPlayer(controller),
+              //gradiente
+
+              //texto
+              Positioned(
+                bottom: 50,
+                left: 20,
+                child: _VideoCaption(caption: widget.caption),
+              ),
+            ],
+          ),
         );
       },
+    );
+  }
+}
+
+class _VideoCaption extends StatelessWidget {
+  final String caption;
+
+  const _VideoCaption({required this.caption});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    final titleStyle = Theme.of(context).textTheme.titleLarge;
+    return SizedBox(
+      width: size.width * 0.6,
+      child: Text(caption, maxLines: 2, style: titleStyle),
     );
   }
 }
