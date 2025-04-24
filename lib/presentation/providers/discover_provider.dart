184bbc950695fbf36e4a8b51b6674b01e5ed7144
lucas juarez <lujuarez@emeal.nttdata.com>
2025-04-24 14:19:10 -0300

    implement video repository in provider and delete call local

--- lib/presentation/providers/discover_provider.dart
+++ lib/presentation/providers/discover_provider.dart
@@ -1,24 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:toktok/domain/entities/video_post.dart';
-import 'package:toktok/infrastructure/models/local_video_model.dart';
-import 'package:toktok/shared/data/local_video_post.dart';
+import 'package:toktok/domain/repositories/video_posts_repository.dart';
 
 class DiscoverProvider extends ChangeNotifier {
-  //todo Repository, DataSource
+  // Repository, DataSource
+  final VideoPostRepository videoRepository;
   bool initialLoading = true;
 
   List<VideoPost> videos = [];
+
+  DiscoverProvider({required this.videoRepository});
 
   Future<void> loadNextPage() async {
     //todo: cargar videos
     // await Future.delayed(const Duration(seconds: 2));
 
-    final List<VideoPost> newVideos =
-        videoPosts
-            .map(
-              (video) => LocalVideoModel.fromJsonMap(video).toVideoPostEntity(),
-            )
-            .toList(); // lo que retorne en este metodo va a ser lo que es newVideos
+    // final List<VideoPost> newVideos =
+    //     videoPosts
+    //         .map(
+    //           (video) => LocalVideoModel.fromJsonMap(video).toVideoPostEntity(),
+    //         )
+    //         .toList(); // lo que retorne en este metodo va a ser lo que es newVideos
+
+    final newVideos = await videoRepository.getTrendingVideosByPage(1);
 
     videos.addAll(newVideos);
     initialLoading = false;
