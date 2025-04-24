e583c4db92232b76b6bfa7143a34d71b8e0927b2
lucas juarez <lujuarez@emeal.nttdata.com>
2025-04-24 11:29:10 -0300

    first steps with datasources and repository

---
+++ lib/domain/datasource/video_posts_datasource.dart
@@ -0,0 +1,7 @@
+import 'package:toktok/domain/entities/video_post.dart';
+
+abstract class VideoPostsDatasource {
+  Future<List<VideoPost>> getTrendingVideosByPage(int page);
+
+  Future<List<VideoPost>> getFavoriteVideosByUser(String userID);
+}
